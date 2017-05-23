f40c3bd4df81d987d3b3f9863df8e915d5cceee3
iMocco <taoxin167@gmail.com>
2017-05-23 22:26:06 +0800

    添加`aliyunoss-webpack-plugin`组件

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -8,6 +8,7 @@ var CopyWebpackPlugin = require('copy-webpack-plugin')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require('extract-text-webpack-plugin')
 var OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin')
+var AliyunossWebpackPlugin = require('aliyunoss-webpack-plugin')
 
 var env = process.env.NODE_ENV === 'testing'
   ? require('../config/test.env')
@@ -94,7 +95,20 @@ var webpackConfig = merge(baseWebpackConfig, {
         to: config.build.assetsSubDirectory,
         ignore: ['.*']
       }
-    ])
+    ]),
+    new AliyunossWebpackPlugin({
+      buildPath: __dirname + '/../dist',
+      region: 'your region',
+      accessKeyId: 'your accessKeyId',
+      accessKeySecret: 'your accessKeySecret',
+      bucket: 'your bucket',
+      deleteAll: true,
+      getObjectHeaders: function(filename) {
+        return {
+          'Cache-Control': 'max-age=2592000'
+        }
+      }
+    })
   ]
 })
 
